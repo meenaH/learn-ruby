8a4cc9fdac83a4c3df10f490a4a9cdbfba40e9e5
meenaH <meena.haq@gmail.com>
2021-11-19 10:42:33 +0000

    ruby boolean

---
+++ Booleans/booleans.rb
@@ -0,0 +1,19 @@
+( 1 == 1 ) && ( 2 == 2 )  # true
+( 1 == 2 ) || ( 2 == 2 ) # true
+!( false ) # true
+
+
+&& # both left and right must evaluate to true
+|| # either left or right must evaluate to true
+! # reverse the reusult
+
+
+
+# test_1 should be true
+test_1 = (2 == 2) && (6 > 3)
+
+# test_2 = should be true
+test_2 = (4 != 3) || false
+
+# test_3 = should be false
+test_3 = (12 <= 13 ) && (5 > 8)
