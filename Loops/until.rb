8cf9e252ca7a3fd8569cb3e2eea41e357a9505b5
meenaH <meena.haq@gmail.com>
2021-11-19 10:30:06 +0000

    until example

---
+++ Loops/until.rb
@@ -0,0 +1,27 @@
+# Example 1
+i = 0 
+until i == 6
+    i +=1
+end
+puts i 
+
+# Example 2
+for i in 1..50
+    i += 1
+    print i
+  end
+  
+# Example 3
+
+i = 0
+until i == 50 do
+    i +=1
+    print i
+end
+
+# Example 4
+
+for i in 1..50
+    print i
+end
+
